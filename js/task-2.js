5f6cde909e818d9a1652d545a3ae76389199f12e
ta_imk <timofejzagirnij@gmail.com>
2025-03-08 22:36:06 +0200

    Fix

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function getShippingMessage(country, price, deliveryFee) {
 
     const totalPrice = price + deliveryFee;
 
-    console.log(`Shipping to ${country} will cost ${totalPrice} credits`);
+    return (`Shipping to ${country} will cost ${totalPrice} credits`);
     
 }
 
